1ec21a1b839f0724f26df19d50ac7b3af58e7839
infotitan <femioladele@infotitans.com>
2014-04-06 13:40:46 +0100

    Finished Base App

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,9 @@
 class Admin < ActiveRecord::Base
-  has_many :equipments
   validates_presence_of :lastname
   validates_presence_of :firstname
   validates_presence_of :email
   validates_presence_of :phone
   validates_presence_of :post
+  has_many :equipment
 end
 
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,9 +1,9 @@
 class Equipment < ActiveRecord::Base
-  belongs_to :admin
   validates_presence_of :name
   validates_presence_of :ip
   validates_presence_of :brand
   validates_presence_of :location
   validates_presence_of :status
   validates_presence_of :admin_id
+  belongs_to :admin
 end
