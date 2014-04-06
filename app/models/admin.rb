208b06ddb456265023c03a8a12a697891e55e62d
infotitan <femioladele@infotitans.com>
2014-04-06 12:01:02 +0100

    Validation

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,9 @@
 class Admin < ActiveRecord::Base
+  has_many :equipments
+  validates_presence_of :lastname
+  validates_presence_of :firstname
+  validates_presence_of :email
+  validates_presence_of :phone
+  validates_presence_of :post
 end
+
